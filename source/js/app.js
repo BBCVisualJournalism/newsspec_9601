bbe5d9d8f7c56643871781aaffa9a9ac548c418b
fewstera <aidan.fewster@bbc.co.uk>
2015-01-30 14:39:24 +0000

    Added data to charts, and aniamted

--- source/js/app.js
+++ source/js/app.js
@@ -1,20 +1,27 @@
-define(['lib/news_special/bootstrap', 'data/data', 'chart'], function (news, data, Chart) {
+define(['lib/news_special/bootstrap', 'data/data', 'chart', 'lib/vendors/bind-polyfill'], function (news, data, Chart) {
 
     function App() {
         /* REUSABLE PROPERTIES */
-        this.el = $('.country-export')
+        this.el = $('.country-export');
         this.exportersEl = this.el.find('.country--input');
         this.exporterChartEl = this.el.find('.chart__exporters');
         this.importerChartEl = this.el.find('.chart__importers');
+        this.seperatorLineEl = this.el.find('.charts--seperator .exports-line');
 
-        this.exporterChart = new Chart(this.exporterChartEl, ['#cce5e5']);
-        this.importerChart = new Chart(this.importerChartEl, ['#c3d699', '#95ba4d', '#689c00']);
+        this.exporterChart = new Chart(this.exporterChartEl, ['#cce5e5'], 'Exporters');
+        this.importerChart = new Chart(this.importerChartEl, ['#c3d699', '#95ba4d', '#689c00'], 'Importers');
 
 
         /* INIT */
         news.sendMessageToremoveLoadingImage();
         var orderedExporters = this.getOrderedExporters();
         this.populateExportersList(orderedExporters);
+        this.exporterChart.setData(orderedExporters).draw();
+        this.updateCharts();
+
+        /* LISTENERS */
+        this.exportersEl.on('change', this.updateCharts.bind(this));
+        
     }
 
     App.prototype = {
@@ -23,7 +30,7 @@ define(['lib/news_special/bootstrap', 'data/data', 'chart'], function (news, dat
             var orderedExporters = [];
             for (var countryName in data) {
                 if (data.hasOwnProperty(countryName) && countryName !== 'other' && countryName !== 'total') {
-                    orderedExporters.push({name: countryName, value: data[countryName]});
+                    orderedExporters.push({name: countryName, value: parseInt(data[countryName].total, 10)});
                 }
             }
             orderedExporters.sort(function (a, b) {
@@ -32,19 +39,44 @@ define(['lib/news_special/bootstrap', 'data/data', 'chart'], function (news, dat
 
             return orderedExporters;
         },
+        getOrderedImportersFrom: function (countryName) {
+            var countryImporters = data[countryName];
+
+            var orderedImporters = [];
+            for (var countryName in countryImporters) {
+                if (countryImporters.hasOwnProperty(countryName) && countryName !== 'total') {
+                    orderedImporters.push({name: countryName, value: parseInt(countryImporters[countryName], 10)});
+                }
+            }
+            orderedImporters.sort(function (a, b) {
+                if(a.name === 'other') {
+                    return 1;
+                }
+                if (b.name === 'other') {
+                    return -1;
+                }
+
+                return b.value - a.value;
+            });
+
+            return orderedImporters;
+        },
         populateExportersList: function (orderedExporters) {
             var self = this;
 
             this.exportersEl.empty();
             $.each(orderedExporters, function (count, exporter) {
                 self.exportersEl.append('<option>' + exporter.name + '</option>');
-            })
+            });
         },
         updateCharts: function () {
-
-
+            var exporterVal = this.exportersEl.val();
+            var country = this.exporterChart.setActive(exporterVal);
+            this.seperatorLineEl.css('top', country.seperatorPosition + 'px');
+            var orderedImoporters = this.getOrderedImportersFrom(country.name);
+            this.importerChart.setData(orderedImoporters).draw();
         }
-        
+
     };
 
     new App();
--- source/js/chart.js
+++ source/js/chart.js
@@ -1,25 +1,130 @@
 define(['lib/news_special/bootstrap'], function (news) {
 
-    function Chart(el, colors) {
+    function Chart(el, colors, header) {
         /* REUSABLE ELEMENTS */
         this.el = el;
         this.colors = colors;
+        this.header = header;
     }
 
     Chart.prototype = {
 
-        setData: function (data) {
+        setData: function (data, total) {
             this.data = data;
+            this.total = total;
+
+            if (!this.total) {
+                this.updateTotal();
+            }
 
             return this;
+        },
+        updateTotal: function () {
+            this.total = 0;
+            this.dataCount = 0;
+
+            for (var country in this.data) {
+                if (this.data.hasOwnProperty(country)) {
+                    this.total += this.data[country].value;
+                    this.dataCount++;
+                }
+            }
+            return this.total;
         },
         draw: function () {
-
+            this.addDimensionsToData();
+            var markup = this.generateMarkUp(this.data);
+            
+            if(this.el.is(':empty')){
+                this.el.html(markup);
+            }else{
+                var self = this;
+                this.el.fadeOut(500, function () {
+                    self.el.html(markup);
+                    self.el.fadeIn(800);
+                });
+            }
+            
+            
+            
             return this;
         },
-        setActive: function () {
+        addDimensionsToData: function () {
+            var chartHeight = 470,
+                headerHeight = 20,
+                barsAreaHeight = chartHeight - headerHeight,
+                barMargin = 8,
+                totalBarMargin = barMargin * this.dataCount,
+                remainingBarHeight = barsAreaHeight - totalBarMargin,
+                rollingOffset = 0; //Contains the offset of each bar
 
+            for (var countryKey in this.data) {
+                if (this.data.hasOwnProperty(countryKey)) {
+                    var country = this.data[countryKey];
 
+                    var barHeight = Math.round(country.value / this.total * remainingBarHeight);
+                    var labelOffet = headerHeight + rollingOffset + (barHeight / 2);
+                    var seperatorPosition = rollingOffset + (barHeight / 2);
+
+                    country.barHeight = barHeight;
+                    country.labelOffet = labelOffet;
+                    country.seperatorPosition = seperatorPosition;
+
+                    rollingOffset += barHeight + barMargin;
+
+                }
+            }
+
+            return this.data;
+        },
+        generateMarkUp: function (data) {
+            var labelsMarkup = '<ol class="chart--labels">';
+            var barsMarkup = '<div class="chart-bars"><h4 class="chart--header">' + this.header + '</h4>';
+            var count = 0;
+            for (var countryKey in this.data) {
+                if (this.data.hasOwnProperty(countryKey)) {
+                    var country = this.data[countryKey];
+                    var color = this.colors[count % this.colors.length];
+                    var valueRounded = Math.round(country.value / 1000000);
+                    /* Capitalise other */
+                    var countryName = (country.name === 'other') ? 'Other' : country.name;
+
+                    labelsMarkup += '<li style="top: ' + country.labelOffet + 'px;"><strong>' + countryName + '</strong> $' + valueRounded + 'm</li>';
+                    barsMarkup += '<div class="chart-bar" style="height: ' + country.barHeight + 'px;background-color: ' + color + '"></div>';
+                
+                    count++;
+                }
+            }
+
+            labelsMarkup += '</ol>';
+            barsMarkup += '</div>';
+
+            return labelsMarkup + barsMarkup;
+        },
+        setActive: function (countryName) {
+            var elementPosition = 0;
+            var selectedCountry = null;
+            for (var countryKey in this.data) {
+                if (this.data.hasOwnProperty(countryKey)) {
+                    if (this.data[countryKey].name === countryName) {
+                        selectedCountry = this.data[countryKey];
+                        break;
+                    } else {
+                        elementPosition++;
+                    }
+                }
+            }
+
+            var labels = this.el.find('.chart--labels li'),
+                bars = this.el.find('.chart-bar');
+
+            labels.removeClass('chart--label__active');
+            bars.removeClass('chart--bar__active');
+
+            $(labels[elementPosition]).addClass('chart--label__active');
+            $(bars[elementPosition]).addClass('chart--bar__active');
+
+            return selectedCountry;
         }
     };
 
